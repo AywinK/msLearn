8c0aa85ebedfe6ad87d1bbc03b4ec0ed93213ce7
Aywin <AywinK@hotmail.com>
2023-10-31 12:54:23 +0000

    .

--- app.ts
+++ app.ts
@@ -314,3 +314,18 @@ let processor = new ProcessIdentityClass<number, string>(100, "hello");
 processor.process();
 processor.value = "100";
 
+class processIdentity<T, U> {
+    private _value: T;
+    private _message: U;
+    constructor(value: T, message: U) {
+        this._value = value;
+        this._message = message;
+    }
+    getIdentity(): T {
+        console.log(this._message);
+        return this._value
+    }
+}
+
+let processor = new processIdentity<number, string>(100, "hello");
+processor.getIdentity();
